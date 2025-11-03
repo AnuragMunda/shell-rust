b820d1762447e28bd58365be4b3dcc1c7277a13e
Anurag Munda <anurag.munda005@gmail.com>
2025-11-03 15:28:34 +0000

    implement exit

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ fn main() {
 
         // User input
         let mut command = String::new();
+        let exit0_command = String::from("exit 0");
+
         io::stdin().read_line(&mut command).unwrap();
+        if command.trim() == exit0_command  {
+            break;
+        }
+
         println!("{}: command not found", command.trim())
     }
 }
