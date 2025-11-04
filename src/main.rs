082ab06d485afa3ef6ee289d56950e1c11d31b1e
Anurag Munda <anurag.munda005@gmail.com>
2025-11-04 06:46:06 +0000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,35 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
-        // exit command
-        let exit0_command = String::from("exit 0");
 
         // Take user input
         io::stdin().read_line(&mut command).unwrap();
+        let trimmed_command = command.trim();
 
-        if command.trim() == exit0_command { // exit if encounter exit command
-            break;
-        } else if command.starts_with("echo ") { // print if encounter echo command
-            let substring = &command.trim()[5..];
-            println!("{substring}");
-        } else if command.starts_with("type ") {
-            let sub_command = &command.trim()[5..];
-            match sub_command {
-                "echo" | "exit" | "type" => println!("{sub_command} is a shell builtin"),
-                _ => println!("{sub_command}: not found"),
-            }
-        } else {
-            println!("{}: command not found", command.trim()); 
+        match trimmed_command {
+            "exit 0" => break, // exit command,
+
+            cmd if trimmed_command.starts_with("echo ") => { // print command
+                let text = &cmd[5..];
+                println!("{text}");
+            },
+
+            cmd if trimmed_command.starts_with("type ") => { // returns the "type" of the command
+                let target = &cmd[5..];
+                match target {
+                    "echo" | "exit" | "type" => println!("{target} is a shell builtin"),
+                _ => println!("{target}: not found"),
+                }
+            },
+
+            "" => {},
+
+            _ => println!("{trimmed_command}: not found"),
         }
     }
 }
