eabf12562954146bc097f095fb622a8eb9aec0d3
Anurag Munda <anurag.munda005@gmail.com>
2025-11-04 06:20:50 +0000

    Implement type

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,27 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // User input
         let mut command = String::new();
+        // exit command
         let exit0_command = String::from("exit 0");
 
+        // Take user input
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == exit0_command {
+
+        if command.trim() == exit0_command { // exit if encounter exit command
             break;
-        } else if command.starts_with("echo") {
+        } else if command.starts_with("echo ") { // print if encounter echo command
             let substring = &command.trim()[5..];
-            println!("{}", substring);
+            println!("{substring}");
+        } else if command.starts_with("type ") {
+            let sub_command = &command.trim()[5..];
+            match sub_command {
+                "echo" | "exit" | "type" => println!("{sub_command} is a shell builtin"),
+                _ => println!("{sub_command}: not found"),
+            }
         } else {
-            println!("{}: command not found", command.trim());
+            println!("{}: command not found", command.trim()); 
         }
     }
 }
+     
