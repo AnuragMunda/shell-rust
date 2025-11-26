1c1e21df67d0de5fe30462937b3e80a2f4c5c7b4
Anurag Munda <anurag.munda005@gmail.com>
2025-11-26 15:42:27 +0000

    locate executable files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::os::unix::fs::PermissionsExt;
+use std::{env, fs};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> Result<(), std::env::VarError> {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -23,15 +25,42 @@ fn main() {
             cmd if trimmed_command.starts_with("type ") => { // returns the "type" of the command
                 let target = &cmd[5..];
                 match target {
-                    "echo" | "exit" | "type" => println!("{target} is a shell builtin"),
-                _ => println!("{target}: not found"),
+                    "echo" | "exit" | "type" => {
+                        println!("{target} is a shell builtin");
+                        continue;
+                    },
+                _ => {},
+                }
+
+                let path = env::var("PATH").unwrap_or_default();
+                let mut found = false;
+
+                for dir in path.split(':') {
+                    let full_path = format!("{}/{}", dir, target);
+
+                    match fs::metadata(&full_path) {
+                        Ok(meta) => {
+                            let mode = meta.permissions().mode();
+
+                            if mode & 0o111 != 0 {
+                                println!("{target} is {full_path}");
+                                found = true;
+                                break;
+                            }
+                        }
+                        Err(_) => {}
+                    }
+                }
+                if !found {
+                    println!("{target}: not found");
                 }
             },
 
-            "" => {},
+            "" => {}, // Do nothing if command is empty
 
-            _ => println!("{trimmed_command}: not found"),
+            _ => println!("{trimmed_command}: command not found"),
         }
     }
+    Ok(())
 }
      
